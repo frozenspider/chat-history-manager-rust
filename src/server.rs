f7a07565996ebcb5b8c06db04eac6fdbe3ff3425
Alex Abdugafarov <fswork90@gmail.com>
2023-11-28 17:39:09 +0500

    Server split onto submodules

--- src/server.rs
+++ src/server.rs
@@ -4,14 +4,14 @@ use std::fmt::Debug;
 use std::fs;
 use std::net::SocketAddr;
 use std::ops::DerefMut;
-use std::path::Path;
 use std::sync::{Mutex, MutexGuard};
 use std::sync::Arc;
 
-use itertools::Itertools;
 use tokio::runtime::Handle;
 use tonic::{Code, Request, Response, Status, transport::Server};
-use tonic::transport::{Channel, Endpoint};
+use tonic::transport::Endpoint;
+
+use myself_chooser::MyselfChooserImpl;
 
 use crate::*;
 use crate::dao::ChatHistoryDao;
@@ -19,8 +19,12 @@ use crate::dao::sqlite_dao::SqliteDao;
 use crate::loader::Loader;
 use crate::protobuf::history::*;
 use crate::protobuf::history::choose_myself_service_client::ChooseMyselfServiceClient;
-use crate::protobuf::history::history_parser_service_server::*;
-use crate::protobuf::history::history_loader_service_server::*;
+use crate::protobuf::history::history_loader_service_server::HistoryLoaderServiceServer;
+use crate::protobuf::history::history_parser_service_server::HistoryParserServiceServer;
+
+mod myself_chooser;
+mod history_parser_service;
+mod history_loader_service;
 
 pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
     tonic::include_file_descriptor_set!("grpc_reflection_descriptor");
@@ -95,276 +99,6 @@ impl<MC: MyselfChooser> ChatHistoryManagerServerTrait<MC> for Arc<Mutex<ChatHist
     }
 }
 
-#[tonic::async_trait]
-impl<MC: MyselfChooser + 'static> HistoryParserService for Arc<Mutex<ChatHistoryManagerServer<MC>>> {
-    async fn parse(&self, req: Request<ParseLoadRequest>) -> TonicResult<ParseResponse> {
-        self.process_request(&req, move |req, self_lock| {
-            let path = Path::new(&req.path);
-            let dao = self_lock.loader.parse(path)?;
-            Ok(ParseResponse {
-                ds: Some(dao.in_mem_dataset()),
-                root_file: String::from(dao.ds_root.to_str().unwrap()),
-                myself: Some(dao.in_mem_myself()),
-                users: (dao.in_mem_users()),
-                cwms: dao.cwms,
-            })
-        })
-    }
-}
-
-macro_rules! with_dao_by_key {
-    ($self:ident, $req:ident, $dao:ident, $code:block) => {
-        $self.process_request_with_dao(&$req, &$req.get_ref().key, |#[allow(unused)] $req, $dao| { $code })
-    };
-}
-
-macro_rules! uuid_from_req { ($req:ident) => { $req.ds_uuid.as_ref().context("Request has no ds_uuid")? }; }
-macro_rules! chat_from_req { ($req:ident) => { $req.chat   .as_ref().context("Request has no chat")? }; }
-
-#[tonic::async_trait]
-impl<MC: MyselfChooser + 'static> HistoryLoaderService for Arc<Mutex<ChatHistoryManagerServer<MC>>> {
-    async fn load(&self, req: Request<ParseLoadRequest>) -> TonicResult<LoadResponse> {
-        self.process_request(&req, move |req, self_lock| {
-            let path = fs::canonicalize(&req.path)?;
-            let path_string = path_to_str(&path)?.to_owned();
-
-            if let Some(dao) = self_lock.loaded_daos.get(&path_string) {
-                let dao = dao.borrow();
-                return Ok(LoadResponse {
-                    file: Some(LoadedFile { key: path_string, name: dao.name().to_owned() })
-                });
-            }
-
-            let dao = self_lock.loader.load(&path)?;
-            let response = LoadResponse {
-                file: Some(LoadedFile { key: path_string.clone(), name: dao.name().to_owned() })
-            };
-            self_lock.loaded_daos.insert(path_string, RefCell::new(dao));
-            Ok(response)
-        })
-    }
-
-    async fn get_loaded_files(&self, req: Request<GetLoadedFilesRequest>) -> TonicResult<GetLoadedFilesResponse> {
-        self.process_request(&req, |_, self_lock| {
-            let files = self_lock.loaded_daos.iter()
-                .map(|(k, dao)| LoadedFile { key: k.clone(), name: dao.borrow().name().to_owned() })
-                .collect_vec();
-            Ok(GetLoadedFilesResponse { files })
-        })
-    }
-
-    async fn save_as(&self, req: Request<SaveAsRequest>) -> TonicResult<LoadedFile> {
-        let mut new_dao: Option<DaoRefCell> = None;
-        let mut new_key: String = String::new();
-
-        let res = with_dao_by_key!(self, req, dao, {
-            let new_storage_path =
-                dao.storage_path().parent().map(|p| p.join(&req.new_folder_name)).context("Cannot resolve new folder")?;
-            if !new_storage_path.exists() {
-                bail!("Path does not exist!")
-            }
-            for entry in fs::read_dir(&new_storage_path)? {
-                let file_name = path_file_name(&entry?.path())?.to_owned();
-                if !file_name.starts_with(".") {
-                    bail!("Directory is not empty! Found {file_name} there")
-                }
-            }
-            let new_db_file = new_storage_path.join(SqliteDao::FILENAME);
-            let sqlite_dao = SqliteDao::create(&new_db_file)?;
-            sqlite_dao.copy_all_from(dao)?;
-            new_key =  path_to_str(&new_db_file)?.to_owned();
-            let name = sqlite_dao.name().to_owned();
-            new_dao = Some(DaoRefCell::new(Box::new(sqlite_dao)));
-            Ok(LoadedFile { key: new_key.clone(), name })
-        });
-
-        if let Some(new_dao) = new_dao {
-            let mut self_lock = lock_or_status(self)?;
-            if self_lock.loaded_daos.contains_key(&new_key) {
-                return Err(Status::new(Code::Internal,
-                                       format!("Key {} is already taken!", new_key)));
-            }
-            self_lock.loaded_daos.insert(new_key, new_dao);
-        }
-
-        res
-    }
-
-    async fn name(&self, req: Request<NameRequest>) -> TonicResult<NameResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(NameResponse { name: dao.name().to_owned() })
-        })
-    }
-
-    async fn storage_path(&self, req: Request<StoragePathRequest>) -> TonicResult<StoragePathResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(StoragePathResponse { path: dao.storage_path().to_str().unwrap().to_owned() })
-        })
-    }
-
-    async fn datasets(&self, req: Request<DatasetsRequest>) -> TonicResult<DatasetsResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(DatasetsResponse { datasets: dao.datasets()? })
-        })
-    }
-
-    async fn dataset_root(&self, req: Request<DatasetRootRequest>) -> TonicResult<DatasetRootResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(DatasetRootResponse {
-                path: dao.dataset_root(uuid_from_req!(req))?.0.to_str().unwrap().to_owned()
-            })
-        })
-    }
-
-    async fn myself(&self, req: Request<MyselfRequest>) -> TonicResult<MyselfResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MyselfResponse { myself: Some(dao.myself(uuid_from_req!(req))?) })
-        })
-    }
-
-    async fn users(&self, req: Request<UsersRequest>) -> TonicResult<UsersResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(UsersResponse { users: dao.users(uuid_from_req!(req))? })
-        })
-    }
-
-    async fn chats(&self, req: Request<ChatsRequest>) -> TonicResult<ChatsResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(ChatsResponse {
-                cwds: dao.chats(uuid_from_req!(req))?
-                    .into_iter()
-                    .map(|cwd| cwd.into())
-                    .collect_vec()
-            })
-        })
-    }
-
-    async fn scroll_messages(&self, req: Request<ScrollMessagesRequest>) -> TonicResult<MessagesResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MessagesResponse {
-                messages: dao.scroll_messages(chat_from_req!(req), req.offset as usize, req.limit as usize)?
-            })
-        })
-    }
-
-    async fn last_messages(&self, req: Request<LastMessagesRequest>) -> TonicResult<MessagesResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MessagesResponse {
-                messages: dao.last_messages(chat_from_req!(req), req.limit as usize)?
-            })
-        })
-    }
-
-    async fn messages_before(&self, req: Request<MessagesBeforeRequest>) -> TonicResult<MessagesResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MessagesResponse {
-                messages: dao.messages_before(chat_from_req!(req),
-                                              MessageInternalId(req.message_internal_id),
-                                              req.limit as usize)?
-            })
-        })
-    }
-
-    async fn messages_after(&self, req: Request<MessagesAfterRequest>) -> TonicResult<MessagesResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MessagesResponse {
-                messages: dao.messages_after(chat_from_req!(req),
-                                             MessageInternalId(req.message_internal_id),
-                                             req.limit as usize)?
-            })
-        })
-    }
-
-    async fn messages_slice(&self, req: Request<MessagesSliceRequest>) -> TonicResult<MessagesResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MessagesResponse {
-                messages: dao.messages_slice(chat_from_req!(req),
-                                             MessageInternalId(req.message_internal_id_1),
-                                             MessageInternalId(req.message_internal_id_2))?
-            })
-        })
-    }
-
-    async fn messages_slice_len(&self, req: Request<MessagesSliceRequest>) -> TonicResult<CountMessagesResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(CountMessagesResponse {
-                messages_count: dao.messages_slice_len(chat_from_req!(req),
-                                                       MessageInternalId(req.message_internal_id_1),
-                                                       MessageInternalId(req.message_internal_id_2))? as i32
-            })
-        })
-    }
-
-    async fn message_option(&self, req: Request<MessageOptionRequest>) -> TonicResult<MessageOptionResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MessageOptionResponse {
-                message: dao.message_option(chat_from_req!(req), MessageSourceId(req.source_id))?
-            })
-        })
-    }
-
-    async fn message_option_by_internal_id(&self, req: Request<MessageOptionByInternalIdRequest>) -> TonicResult<MessageOptionResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(MessageOptionResponse {
-                message: dao.message_option_by_internal_id(chat_from_req!(req), MessageInternalId(req.internal_id))?
-            })
-        })
-    }
-
-    async fn is_loaded(&self, req: Request<IsLoadedRequest>) -> TonicResult<IsLoadedResponse> {
-        with_dao_by_key!(self, req, dao, {
-            Ok(IsLoadedResponse {
-                is_loaded: dao.is_loaded(&Path::new(&req.storage_path))
-            })
-        })
-    }
-
-    async fn close(&self, req: Request<CloseRequest>) -> TonicResult<CloseResponse> {
-        self.process_request(&req, |req, self_lock| {
-            let dao = self_lock.loaded_daos.remove(&req.key);
-            Ok(CloseResponse { success: dao.is_some() })
-        })
-    }
-}
-
-struct ChooseMyselfImpl {
-    runtime_handle: Handle,
-    channel: Channel,
-}
-
-impl MyselfChooser for ChooseMyselfImpl {
-    fn choose_myself(&self, users: &[User]) -> Result<usize> {
-        let users = users.to_vec();
-        let handle = self.runtime_handle.clone();
-        let channel = self.channel.clone();
-
-        // We cannot use the current thread since when called via RPC, current thread is already used for async tasks.
-        // We're unwrapping join() to propagate panic.
-        std::thread::spawn(move || {
-            let len = users.len();
-            let choose_myself_future = async move {
-                let mut client = ChooseMyselfServiceClient::new(channel);
-                log::info!("Sending ChooseMyselfRequest");
-                client.choose_myself(ChooseMyselfRequest { users })
-                    .await.map_err(|status| anyhow!("{}", status.message()))
-            };
-
-            let spawned = handle.spawn(choose_myself_future);
-            let response = handle.block_on(spawned).map(|b| b)?;
-            log::info!("Got response: {:?}", response);
-
-            let response = response?.get_ref().picked_option;
-            if response < 0 {
-                err!("Choice aborted!")
-            } else if response as usize >= len {
-                err!("Choice out of range!")
-            } else {
-                Ok(response as usize)
-            }
-        }).join().unwrap()
-    }
-}
-
 // https://betterprogramming.pub/building-a-grpc-server-with-rust-be2c52f0860e
 #[tokio::main]
 pub async fn start_server<H: HttpClient>(port: u16, http_client: &'static H) -> EmptyRes {
@@ -373,7 +107,7 @@ pub async fn start_server<H: HttpClient>(port: u16, http_client: &'static H) -> 
     let remote_port = port + 1;
     let runtime_handle = Handle::current();
     let lazy_channel = Endpoint::new(format!("http://127.0.0.1:{remote_port}"))?.connect_lazy();
-    let myself_chooser = ChooseMyselfImpl {
+    let myself_chooser = MyselfChooserImpl {
         runtime_handle: runtime_handle.clone(),
         channel: lazy_channel.clone(),
     };
@@ -406,7 +140,7 @@ pub async fn debug_request_myself(port: u16) -> Result<usize> {
     let conn_port = port + 1;
     let runtime_handle = Handle::current();
     let lazy_channel = Endpoint::new(format!("http://127.0.0.1:{conn_port}"))?.connect_lazy();
-    let chooser = ChooseMyselfImpl {
+    let chooser = MyselfChooserImpl {
         runtime_handle,
         channel: lazy_channel,
     };
---
+++ src/server/myself_chooser.rs
@@ -0,0 +1,44 @@
+use tokio::runtime::Handle;
+use tonic::transport::Channel;
+
+use crate::*;
+
+use super::*;
+
+pub struct MyselfChooserImpl {
+    pub runtime_handle: Handle,
+    pub channel: Channel,
+}
+
+impl MyselfChooser for MyselfChooserImpl {
+    fn choose_myself(&self, users: &[User]) -> Result<usize> {
+        let users = users.to_vec();
+        let handle = self.runtime_handle.clone();
+        let channel = self.channel.clone();
+
+        // We cannot use the current thread since when called via RPC, current thread is already used for async tasks.
+        // We're unwrapping join() to propagate panic.
+        std::thread::spawn(move || {
+            let len = users.len();
+            let choose_myself_future = async move {
+                let mut client = ChooseMyselfServiceClient::new(channel);
+                log::info!("Sending ChooseMyselfRequest");
+                client.choose_myself(ChooseMyselfRequest { users })
+                    .await.map_err(|status| anyhow!("{}", status.message()))
+            };
+
+            let spawned = handle.spawn(choose_myself_future);
+            let response = handle.block_on(spawned).map(|b| b)?;
+            log::info!("Got response: {:?}", response);
+
+            let response = response?.get_ref().picked_option;
+            if response < 0 {
+                err!("Choice aborted!")
+            } else if response as usize >= len {
+                err!("Choice out of range!")
+            } else {
+                Ok(response as usize)
+            }
+        }).join().unwrap()
+    }
+}
