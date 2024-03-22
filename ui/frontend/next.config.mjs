5bba3c552817af297c1dcd98f2170944e0e71fe1
Alex Abdugafarov <fswork90@gmail.com>
2024-03-22 19:45:13 +0500

    UI hello world moved to Next.js

---
+++ ui/frontend/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Ensure Next.js uses SSG instead of SSR
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: 'export',
+};
+
+export default nextConfig;
